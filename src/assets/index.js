dd802b04ae033b523d3188b5e9088ac5c90b00bd
Srikanth <91842381+srikanths2046@users.noreply.github.com>
2022-09-28 21:05:21 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,57 @@
+import petvio from "./petvio.svg";
+import pets1 from "./pets1.png";
+import close from "./close.png";
+import menu from "./menu.png";
+import catFood from "./cat_food.png";
+import collars from "./dog_collars.png";
+import leashes from "./leashes.png";
+import petFoods from "./pet_food_shovel.png";
+import bowl from "./bowl.png";
+import bone from "./bone_toy.jpg";
+import facebook from "./facebook.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import twitter from "./twitter.png";
+import pets2 from "./pets2.png";
+import Onlypets from "./Onlypets.png";
+import Ani from "./Ani.png";
+import Vet from "./vet.jpg";
+import PetCare from "./petCare.jpg";
+import Groom from "./groom.jpg";
+import Res from "./res.jpg";
+import logo from "./logo.png";
+import quotes from "./quotes.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import bg from "./bg.jpg";
+
+export {
+    petvio,
+    pets1,
+    pets2,
+    menu,
+    close,
+    Onlypets,
+    catFood,
+    collars,
+    leashes,
+    petFoods,
+    bowl,
+    bone,
+    facebook,
+    instagram,
+    linkedin,
+    twitter,
+    Ani,
+    Vet,
+    PetCare,
+    Groom,
+    Res,
+    logo,
+    quotes,
+    people01,
+    people02,
+    people03,
+    bg,
+};
