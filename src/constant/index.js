92d77217fffe4cfb87d8cfdf3bf85117f65afb65
Srikanth <91842381+srikanths2046@users.noreply.github.com>
2022-09-28 21:08:06 +0530

    Add files via upload

---
+++ src/constant/index.js
@@ -0,0 +1,202 @@
+import { catFood,
+    collars,
+    leashes,
+    petFoods,
+    bowl,
+    bone, facebook, instagram, linkedin, twitter, people01, people02, people03, } from "../assets";
+
+export const NavLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "about",
+        title: "About us",
+    },
+    {
+        id: "services",
+        title: "Our services",
+    },
+    {
+        id: "shop",
+        title: "Shop",
+    },
+    {
+        id: "contact",
+        title: "Contact us",
+    },
+];
+
+export const services = [
+    {
+        id: "vetnearby",
+        title: "🐾Vets Near by",
+    },
+    {
+        id: "rescue",
+        title: "🐾Rescue Center",
+    },
+    {
+        id: "abuse",
+        title: "🐾Report Animal Abuse",
+    },
+];
+
+const products = [
+    {
+        id: "product-1",
+        image: catFood,
+        title: "Cat Food",
+        content: "Delicious Cat treat",
+    },
+    {
+        id: "product-2",
+        image: petFoods,
+        title: "Dog Food",
+        content: "Delicious Dog treat",
+    },
+    {
+        id: "product-3",
+        image: leashes,
+        title: "Dog Leashes",
+        content: "Quality dog leshes for your beloved dogs",
+    },
+    {
+        id: "product-4",
+        image: bowl,
+        title: "Pet Food Bowls",
+        content: "Quality Food bowls to feed your beloved pets",
+    },
+    {
+        id: "product-5",
+        image: bone,
+        title: "Bone Toy",
+        content: "Chewable Toys for your pets",
+    },
+    {
+        id: "product-6",
+        image: collars,
+        title: "Pet Collars",
+        content: "Quality pet collars for your pets",
+    },
+];
+
+export const feedback = [
+    {
+      id: "feedback-1",
+      content:
+        "Just say anything, George, say what ever's natural, the first thing that comes to your mind. Well it's gonna cost you. How much money you got on you?",
+      name: "Herman Jensen",
+      title: "Founder & Leader",
+      img: people01,
+    },
+    {
+      id: "feedback-2",
+      content:
+        "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      name: "Steve Mark",
+      title: "Founder & Leader",
+      img: people02,
+    },
+    {
+      id: "feedback-3",
+      content:
+        "It is usually people in the money business, finance, and international trade that are really rich.",
+      name: "Kenn Gallagher",
+      title: "Founder & Leader",
+      img: people03,
+    },
+  ];
+  
+
+export default products
+
+export const footerLinks = [
+        {
+        title: "Useful Links",
+        links: [
+            {
+            name: "Content",
+            link: "",
+            },
+            {
+            name: "How it Works",
+            link: "",
+            },
+            {
+            name: "Create",
+            link: "",
+            },
+            {
+            name: "Explore",
+            link: "",
+            },
+            {
+            name: "Terms & Services",
+            link: "",
+            },
+        ],
+        },
+        {
+        title: "Community",
+        links: [
+            {
+            name: "Help Center",
+            link: "",
+            },
+            {
+            name: "Partners",
+            link: "",
+            },
+            {
+            name: "Suggestions",
+            link: "",
+            },
+            {
+            name: "Blog",
+            link: "",
+            },
+            {
+            name: "Newsletters",
+            link: "",
+            },
+        ],
+        },
+        {
+        title: "Partner",
+        links: [
+            {
+            name: "Our Partner",
+            link: "",
+            },
+            {
+            name: "Become a Partner",
+            link: "",
+            },
+        ],
+        },
+    ];
+    
+    export const socialMedia = [
+        {
+        id: "social-media-1",
+        icon: instagram,
+        link: "https://www.instagram.com/",
+        },
+        {
+        id: "social-media-2",
+        icon: facebook,
+        link: "https://www.facebook.com/",
+        },
+        {
+        id: "social-media-3",
+        icon: twitter,
+        link: "https://www.twitter.com/",
+        },
+        {
+        id: "social-media-4",
+        icon: linkedin,
+        link: "https://www.linkedin.com/",
+        },
+    ];
